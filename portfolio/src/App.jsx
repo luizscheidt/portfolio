c999c2a40990baa030f7a5209ad83246291cdfa7
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-03-04 14:46:12 -0300

    Changing the font and adding links to the buttons

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,22 @@
 import FirstSection from "./FirstSection"
 import Navbar from "./Navbar"
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 
+      'Fjalla One',
+      fontWeight: 700
+  },});
 
 function App() {
 
   return (
     <>
+    <ThemeProvider theme={theme}>
       <Navbar/>
       <FirstSection/>
+      </ThemeProvider>
     </>
   )
 }
--- portfolio/src/MainCard.jsx
+++ portfolio/src/MainCard.jsx
@@ -7,22 +7,29 @@ export default function MainCard(){
     return (
         <Box
         marginTop={20}
-        
+        marginLeft={5}
         >
             <Typography variant="h4" component="h1">
-                Meu nome é...
+                My name is...
             </Typography>
             <Typography variant="h2" component="h2">
                 Luiz Felipe Eberhardt Buss Scheidt
             </Typography>
             <Typography variant="h3" component="h3">
-                Desenvolvedor Web
+                Web Developer - Brasil
+            </Typography>
+            <Typography variant="h5" component="h4">
+                Student at UFSC
             </Typography>
             <Fab color="dark" aria-label="add" sx={{m:1}}>
+                <a href="https://github.com/luizscheidt">
                 <GitHubIcon />
+                </a>                
             </Fab>
             <Fab color="dark" aria-label="add">
+            <a href="https://www.linkedin.com/in/luiz-felipe-scheidt-a1087624b/">
                 <LinkedInIcon />
+            </a>
             </Fab>
         </Box>
         )
