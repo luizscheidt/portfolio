3398eb8efd0d2dd4a025a185643e2a4dbbda2e8f
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-03-04 19:12:59 -0300

    Making the first section resposive

--- portfolio/src/FirstSection.jsx
+++ portfolio/src/FirstSection.jsx
@@ -1,5 +1,7 @@
 import {Avatar, Grid} from "@mui/material"
+
 import "./FirstSection.css"
+
 import MainCard from "./MainCard"
 import LogoLine from "./LogoLine"
 
@@ -7,18 +9,12 @@ export default function FirstSection(){
     return (
         <Grid container spacing={1}>
            <Grid item md={3} xs={12} className="FirstSection">
-                <Avatar
-                    alt="Profile Pic"
-                    src="./IMG_7303.jpg"
-                    sx={ { width: "100%", height: "55%", mt: 20 }}
-                    className="Avatar"
-                    
-                />
+                <img src="./IMG_7303.jpg" alt="Profile Pic" className="Avatar" />
            </Grid>
-           <Grid item xs={8}>
+           <Grid item lg={8} md={9}>
                 <MainCard/>
            </Grid>
-           <Grid item xs={1} sx={{mt: 10}}>
+           <Grid item xl={1} md={12} sx={{mt: 10}}>
                 <LogoLine/>
            </Grid>
         </Grid>
