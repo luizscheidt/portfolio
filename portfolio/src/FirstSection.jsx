f1b6085650c21853888d0c05aeee4bfefe5f258c
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-03-05 10:49:24 -0300

    Tweaking the LogoLine responsiveness

--- portfolio/src/FirstSection.jsx
+++ portfolio/src/FirstSection.jsx
@@ -1,4 +1,4 @@
-import {Avatar, Grid} from "@mui/material"
+import { Grid } from "@mui/material"
 
 import "./FirstSection.css"
 
@@ -6,6 +6,8 @@ import MainCard from "./MainCard"
 import LogoLine from "./LogoLine"
 
 export default function FirstSection(){
+    let width = window.innerWidth;
+
     return (
         <Grid container spacing={1}>
            <Grid item md={4} xs={12} className="FirstSection">
@@ -13,9 +15,12 @@ export default function FirstSection(){
            </Grid>
            <Grid item lg={7} md={8}>
                 <MainCard/>
+                <Grid className="VerticalLogos">
+                    <LogoLine/>
+                </Grid>
            </Grid>
-           <Grid item lg={1} md={12} sx={{mt: 10}}>
-                <LogoLine/>
+           <Grid item lg={1} md={12} sx={{mt: 10}} className="HorizontalLogos">
+                <LogoLine />
            </Grid>
         </Grid>
     )
