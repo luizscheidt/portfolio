996010df3e28d098a55152f0662a117b96bb12b7
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-03-07 10:52:20 -0300

    First skecth for the about me page

--- portfolio/src/About.jsx
+++ portfolio/src/About.jsx
@@ -1,5 +1,22 @@
+import { Grid, Typography, Stack, Button } from "@mui/material"
+
+import "./About.css"
+
 export default function (){
     return (
-        <h1>OIIIIIIIIIIII</h1>
+        <Grid container spacing={1} className="ThirdSection" sx={{mt:0.01, height: "100vh"}}>
+            <Grid item xs={4}>
+            <Typography variant="h2" component="h2">
+                Who am I?
+            </Typography>
+            </Grid>
+            <Grid item xs={4}>
+            </Grid>
+            <Grid item xs={4}>
+            <Typography variant="h2" component="h2">
+                Quem sou eu?
+            </Typography>
+            </Grid>
+        </Grid>
     )
 }
